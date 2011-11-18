3da976486bd109d48e7c3c43619707a0ab30c583
Michael Goerz <goerz@physik.uni-kassel.de>
2011-11-18 18:43:43 +0100

    Added lqdel script

--- LPBS/JobIDManager.py
+++ LPBS/JobIDManager.py
@@ -19,11 +19,13 @@
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
 
-import random
+""" Manage Job IDs """
+
 import os
 import sys
-
-""" Manage Job IDs """
+import signal
+import logging
+from glob import glob
 
 def get_new_job_id(options):
     sequencefile = os.path.join(os.environ['LPBS_HOME'], 
@@ -34,7 +36,7 @@ def get_new_job_id(options):
         sequence = int(sequencefile_fh.read())
         sequencefile_fh.close()
     except IOError, e:
-        print >>sys.stderr, "Could not read from %s:\n%s" % (sequencefile, e)
+        print >> sys.stderr, "Could not read from %s:\n%s" % (sequencefile, e)
         return None
     sequence = sequence + 1
     # write new sequence number back to sequence file
@@ -43,7 +45,7 @@ def get_new_job_id(options):
         sequencefile_fh.write(str(sequence))
         sequencefile_fh.close()
     except IOError, e:
-        print >>sys.stderr, "Could not write to %s:\n%s" % (sequencefile, e)
+        print >> sys.stderr, "Could not write to %s:\n%s" % (sequencefile, e)
         return None
     id_host = options.config.get("Server", 'hostname')
     id_domain = options.config.get("Server", 'domain')
@@ -60,9 +62,39 @@ def set_lock(job_id, pid):
     lock.write(str(pid))
     lock.close()
 
+
 def release_lock(job_id):
     lockfile = os.path.join(os.environ['LPBS_HOME'], "%s.lock" % job_id) 
     try:
         os.unlink(lockfile)
     except OSError:
         pass
+
+
+def pid_for_job_id(job_id):
+    """ For a given Job_ID, return the process ID, or None if the given job id
+        does not exist or is not accessible
+    """
+    lock_files = glob(os.path.join(os.environ['LPBS_HOME'], '*.lock'))
+    for lock_file in lock_files:
+        if os.path.basename(lock_file).startswith(job_id):
+            try:
+                fh = open(lock_file)
+                pid = fh.read()
+                fh.close()
+                return(int(pid))
+            except IOError:
+                return None
+    return None
+
+
+def send_sig_to_job_id(job_id, sig=signal.SIGTERM):
+    """ Send a sig to the given job id """
+    pid = pid_for_job_id(job_id)
+    if pid is not None:
+        try:
+            os.kill(pid, sig)
+            logging.info("Sent signal %s to job %s (PID %s)"
+                         % (sig, job_id, pid))
+        except OSError:
+            pass
