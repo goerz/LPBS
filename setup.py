9886e49367eff4dc3dacd3021898b83afe690d4c
Michael Goerz <goerz@physik.uni-kassel.de>
2011-11-18 19:29:49 +0100

    Added lqdel script to distribution

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='lpbs',
-      version='0.0.2',
+      version='0.0.3',
       description='Local Portable Batch System',
       author='Michael Goerz',
       author_email='goerz@physik.uni-kassel.de',
       url='http://michaelgoerz.net',
       license='GPL',
       packages=['LPBS'],
-      scripts=['lqsub']
+      scripts=['lqsub', 'lqdel']
      )
