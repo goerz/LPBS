03b1d4e6f4daabf08fe387045990614ea91676a5
Michael Goerz <goerz@physik.uni-kassel.de>
2011-11-19 00:10:29 +0100

    Added lqstat script

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='lpbs',
-      version='0.0.3',
+      version='0.4',
       description='Local Portable Batch System',
       author='Michael Goerz',
       author_email='goerz@physik.uni-kassel.de',
       url='http://michaelgoerz.net',
       license='GPL',
       packages=['LPBS'],
-      scripts=['lqsub', 'lqdel']
+      scripts=['lqsub', 'lqdel', 'lqstat']
      )
