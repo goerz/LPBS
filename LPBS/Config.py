2eef6ad8644e840b171bc59012ca259360fa23f6
Michael Goerz <goerz@physik.uni-kassel.de>
2011-12-29 20:26:43 +0100

    Added documentation to default config

--- LPBS/Config.py
+++ LPBS/Config.py
@@ -29,29 +29,81 @@ from StringIO import StringIO
 
 DEFAULTS = StringIO("""\
 [Server]
+
+# Full hostname of submission server (hostname.domain). Will be made available
+# to running job through the environment variable PBS_SERVER. Job IDs will end
+# in the server hostname
+
 hostname: localhost
 domain: local
+
 
 [Node]
+
+# Full hostname of the execution node (hostname.domain). Will be made available
+# to running job through the environment variable PBS_O_HOST. Since LPBS is
+# designed to execute jobs locally, the settings here should in general be
+# identical to those in the [Server] section
+
 hostname: localhost
 domain: local
 
+
 [LPBS]
+
+# Setting for job execution.
+# If 'username_in_jobid' is enabled, the job IDs will have the form
+# 'seqnr.user.hostname.domain' where 'user' is the username of the user
+# submitting the job.
+# The file given in 'sequence_file' is used for keeping track of the 'seqnr'
+# appearing in the job ID.
+# The file given in 'logfile' is used for logging all LPBS events. Both
+# 'sequence_file' and 'logfile' are relative to $LPBS_HOME.
+
 username_in_jobid: 0
 sequence_file: sequence
 logfile: lpbs.log
 
+
 [Scratch]
+
+# Settings for the scratch space provided to jobs. 'scratch_root' defines a
+# location where jobs should write temporary data. If given as a relative path,
+# it is relative to $LPBS_HOME. Environment variables will be expanded at the
+# time of the job submission.
+# If the value of # 'create_jobid_folder' is set to 1, a folder with the name of
+# the full job ID is created inside scratch_root. This folder is automatically
+# deleted when the job ends, unless 'keep_scratch' is set to 1. If the job
+# failed, the scratch will not be deleted, unless 'delete_failed_scratch' is set
+# to 1.
+
 scratch_root: $SCRATCH_ROOT
-create_jobid_folder: 1
+create_jobid_folder: 0
 keep_scratch: 0
 delete_failed_scratch: 0
 
+
 [Notification]
+
+# Settings on how the user should be be notified about events such as the start
+# and end of a job. If sent_mail is set to 1, emails will be sent for
+# notifications depending on the value of the '-m' option to lqsub. If
+# 'send_growl' is set to 1, Growl (http://growl.info) is used for notification
+# on MacOS X. Notifications via Growl do not take into account the '-m' options
+# during job submission.
+
 send_mail: 0
 send_growl: 0
 
+
 [Mail]
+
+# SMTP settings for email notifications. Notification emails will be sent from
+# the address given by the 'from' option. The SMTP server given in 'smtp' is
+# used for sending the emails, if 'authenticate' is set to 1, authentication is
+# done with the given 'username' and 'password'. If 'tls' is 1, TLS encryption
+# will be used.
+
 from: nobody@example.org
 smtp: smtp.example.com:587
 username: user
@@ -59,12 +111,27 @@ password: secret
 authenticate: 0
 tls: 1
 
+
 [Growl]
-sticky: 1
+
+# Settings for Growl notifications. Notifications are sent to either localhost
+# or a remote host via the GNTP protocol. The 'hostname' setting gives the
+# address and port of the Growl server, the given 'password' is used for
+# authentication (note that if sending to localhost, no authentication is
+# necessary). If 'sticky' is set to 1, the Growl notifications will be sticky.
+# It is possible to send notifications to more than one host. In this case, both
+# 'hostname' and 'password' should be a comma-separated list of values, with
+# each item corresponding to one host.
+
 hostname: localhost:23053
 password: secret
+sticky: 0
+
 
 [Log]
+
+# 'logfile' gives the name of the central log file, relative to $LPBS_HOME.
+
 logfile: lpbs.log
 """)
 
